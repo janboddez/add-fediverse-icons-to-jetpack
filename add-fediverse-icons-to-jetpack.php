1ef672b322793ba10278a6ccf80cc6ddd97b2f54
Jan Boddez <jan.boddez@gmail.com>
2022-09-23 22:58:46 +0200

    Block theme functions

--- add-fediverse-icons-to-jetpack.php
+++ add-fediverse-icons-to-jetpack.php
@@ -47,6 +47,7 @@ class Fediverse_Icons_Jetpack {
 	private function __construct() {
 		add_action( 'wp_footer', array( $this, 'print_icons' ), 10000 ); // This prio should have our icons land below Jetpack's.
 		add_filter( 'walker_nav_menu_start_el', array( $this, 'apply_icon' ), 100, 4 ); // Important! This prio should make this callback run after Jetpack's.
+		add_action( 'init', array( $this, 'handle_block_themes' ), 9 );
 	}
 
 	/**
@@ -112,6 +113,94 @@ class Fediverse_Icons_Jetpack {
 		}
 
 		return $item_output;
+	}
+
+
+	/**
+	 * Overrides WordPress' `core/social-link` render callback.
+	 */
+	public function handle_block_themes() {
+		remove_action( 'init', 'register_block_core_social_link' );
+
+		add_action( 'init', function() {
+			remove_action( 'init', 'gutenberg_register_block_core_social_link', 20 );
+		} );
+
+		add_action( 'init', function() {
+			register_block_type_from_metadata(
+				ABSPATH . WPINC . '/blocks/social-link',
+				array(
+					'render_callback' => array( $this, 'render_block_core_social_link' ),
+				)
+			);
+		} );
+	}
+
+	/**
+	 * Pretty much lifted from WP core. It'd be nice if we had a simple PHP
+	 * filter for this type of thing.
+	 *
+	 * @todo: Look into block variations?
+	 */
+	public function render_block_core_social_link( $attributes, $content, $block ) {
+		$open_in_new_tab = isset( $block->context['openInNewTab'] ) ? $block->context['openInNewTab'] : false;
+
+		$service     = ( isset( $attributes['service'] ) ) ? $attributes['service'] : 'Icon';
+		$url         = ( isset( $attributes['url'] ) ) ? $attributes['url'] : false;
+		$label       = ( isset( $attributes['label'] ) ) ? $attributes['label'] : block_core_social_link_get_name( $service );
+		$show_labels = array_key_exists( 'showLabels', $block->context ) ? $block->context['showLabels'] : false;
+
+		// Don't render a link if there is no URL set.
+		if ( ! $url ) {
+			return '';
+		}
+
+		/**
+		 * Prepend emails with `mailto:` if not set.
+		 * The `is_email` returns false for emails with schema.
+		 */
+		if ( is_email( $url ) ) {
+			$url = 'mailto:' . $url;
+		}
+
+		/**
+		 * Prepend URL with https:// if it doesn't appear to contain a scheme
+		 * and it's not a relative link starting with //.
+		 */
+		if ( ! parse_url( $url, PHP_URL_SCHEME ) && ! str_starts_with( $url, '//' ) ) {
+			$url = 'https://' . $url;
+		}
+
+		$rel_target_attributes = '';
+		if ( $open_in_new_tab ) {
+			$rel_target_attributes = 'rel="noopener nofollow" target="_blank"';
+		}
+
+		$icon = block_core_social_link_get_icon( $service );
+
+		// Hardcoded, for now.
+		// @todo: Add the other platforms. Run this only when a block theme's active?
+		if ( 'Mastodon' === $label ) {
+			$icon = '<svg class="icon icon-mastodon" aria-hidden="true" role="img"> <use href="#icon-mastodon" xlink:href="#icon-mastodon"></use> </svg>';
+		} elseif ( 'Pixelfed' === $label ) {
+			$icon = '<svg class="icon icon-pixelfed" aria-hidden="true" role="img"> <use href="#icon-pixelfed" xlink:href="#icon-pixelfed"></use> </svg>';
+		}
+
+		$wrapper_attributes = get_block_wrapper_attributes(
+			array(
+				'class' => 'wp-social-link wp-social-link-' . $service,
+				'style' => block_core_social_link_get_color_styles( $block->context ),
+			)
+		);
+
+		$link  = '<li ' . $wrapper_attributes . '>';
+		$link .= '<a href="' . esc_url( $url ) . '" ' . $rel_target_attributes . ' class="wp-block-social-link-anchor">';
+		$link .= $icon;
+		$link .= '<span class="wp-block-social-link-label' . ( $show_labels ? '' : ' screen-reader-text' ) . '">';
+		$link .= esc_html( $label );
+		$link .= '</span></a></li>';
+
+		return $link;
 	}
 }
 
